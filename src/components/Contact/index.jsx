e20753d065b987a0b39cab703ecca39ca4853b1c
cderoure-websites <123341219+cderoure-websites@users.noreply.github.com>
2023-01-22 19:47:38 -0800

    feat: add content to contact page

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -3,9 +3,30 @@ import React from 'react';
 import './index.scss';
 
 const Contact = () => {
+  const submitForm = () => {
+    // TODO
+  };
+  
   return (
     <div className="contact">
-      Coming Soon!
+      <div className="contact-title">
+        Contact Us
+      </div>
+      <div className="contact-body">
+        <b>Anke contact:</b> TBC
+      </div>
+      <div className="contact-body">
+        <b>Myles contact:</b> TBC
+      </div>
+      <div className="contact-form">
+        Something needs to be clarified? 
+        If you have any question or if you want to leave a comment, do it here and we will receive it via e-mail. Thanks for your message!
+        <div className="contact-form-field">
+          <textarea rows="10" cols="40" id="form" />
+        </div>
+        <button onClick={submitForm} className="contact-submit">Submit</button>
+      </div>
+
     </div>
   )
 };
--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -3,9 +3,81 @@ import React from 'react';
 import './index.scss';
 
 const Quiz = () => {
+  const submitQuiz = () => {
+    // TODO
+  };
+
   return (
     <div className="quiz">
-      Coming Soon!
+      <div className="quiz-title">
+        How well do you know us?
+      </div>
+      <div className="quix-description">
+      Do you really think that you actually know everything about us? We have created a test, the one who gets most of them right will receive a prize on our wedding day. Do you want it?
+      </div>
+      <div className="quiz-form">
+        <div className="quiz-form-field">
+          <label className="quiz-form-field-label">
+            Which coastal town on the East Coast of Australia did we meet?
+          </label>
+          <input id="" value=""/>
+        </div>
+        <div className="quiz-form-field">
+          <label className="quiz-form-field-label">
+            How did they get talking?
+          </label>
+          <input id="" value=""/>
+        </div>
+        <div className="quiz-form-field">
+          <label className="quiz-form-field-label">
+            Where did Myles propose?
+          </label>
+          <input id="" value=""/>
+        </div>
+        <div className="quiz-form-field">
+          <label className="quiz-form-field-label">
+            How many countries have we travelled together?
+          </label>
+          <input id="" value=""/>
+        </div>
+        <div className="quiz-form-field">
+          <label className="quiz-form-field-label">
+            What is their favourite type of holiday?
+          </label>
+          <input id="" value=""/>
+        </div>
+        <div className="quiz-form-field">
+          <label className="quiz-form-field-label">
+            Who plays more golf?
+          </label>
+          <input id="" value=""/>
+        </div>
+        <div className="quiz-form-field">
+          <label className="quiz-form-field-label">
+            Who said 'I love you' first?
+          </label>
+          <input id="" value=""/>
+        </div>
+        <div className="quiz-form-field">
+          <label className="quiz-form-field-label">
+            How did the bride ask the groom out?
+          </label>
+          <input id="" value=""/>
+        </div>
+        <div className="quiz-form-field">
+          <label className="quiz-form-field-label">
+            Who wanted to marry someone from an Island?
+          </label>
+          <input id="" value=""/>
+        </div>
+        <div className="quiz-form-field">
+          <label className="quiz-form-field-label">
+            Which are their dream holiday destinations?
+          </label>
+          <input id="" value=""/>
+        </div>
+      </div>
+      <button onClick={submitQuiz} className="quiz-submit">Submit</button>
     </div>
   )
 };
