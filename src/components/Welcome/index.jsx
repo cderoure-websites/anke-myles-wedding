9ed53968bb81bf3e05a6158e0ad14ec3568a4a83
cderoure-websites <123341219+cderoure-websites@users.noreply.github.com>
2023-01-22 20:15:43 -0800

    feat: update welcome screen

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -13,31 +13,38 @@ const calculateDaysToGo = () => {
 const Welcome = ({ onClickRSVP }) => {
   return (
     <div className="welcome">
+      <div className="welcome-lhs">
+        <p className="welcome-content">
+          Yes! We are getting married!
+          We are so excited! We are in cloud nine and we want to share our love with you. We are planning a wedding that will make history, in which you will have lots of fun.
+          As we wait for the big day to arrive, we have created this website with lots of sections to keep you up-to-date of everything and to share our love story.
+          Just one important thing, in the attendance section you can confirm whether or not you will be at our wedding with us. Please, confirm as soon as possible, so we can arrange everything much faster.
+          Enjoy the site and see you soon, lots of love!
+        </p>
 
-      <div className="welcome-countdown">
-        {calculateDaysToGo()} days to go!
+        <div className="welcome-signature">
+          Myles & Anke
+        </div>
       </div>
 
-      <div className="welcome-title">
-        · 19th August 2023 ·
-      </div>
-      <p className="welcome-content">
-        Yes! We are getting married!
-        We are so excited! We are in cloud nine and we want to share our love with you. We are planning a wedding that will make history, in which you will have lots of fun.
-        As we wait for the big day to arrive, we have created this website with lots of sections to keep you up-to-date of everything and to share our love story.
-        Just one important thing, in the attendance section you can confirm whether or not you will be at our wedding with us. Please, confirm as soon as possible, so we can arrange everything much faster.
-        Enjoy the site and see you soon, lots of love!
-      </p>
+      <div className="welcome-rhs">
+        <div className="welcome-countdown">
+          {calculateDaysToGo()} days to go!
+        </div>
 
-      <div className="welcome-signature">
-        Myles & Anke
+        <div className="welcome-date">
+          · 19th August 2023 ·
+        </div>
+        
+        <button className="welcome-rsvp" onClick={onClickRSVP}>
+          RSVP
+        </button>
+        
+        <div className="welcome-rsvp-warning">
+          Please RSVP by XX/XX/2023
+        </div>
       </div>
-      <button className="welcome-rsvp" onClick={onClickRSVP}>RSVP</button>
       
-
-      <div className="welcome-rsvp-warning">
-        Please RSVP by XX/XX/2023
-      </div>
     </div>
   );
 };
