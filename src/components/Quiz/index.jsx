6e8daf5f67919cb6f2002da50bfd1e153244fbd9
cderoure+website <catderoure@gmail.com>
2023-02-12 18:20:43 -0800

    feat: update quiz

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -1,20 +1,20 @@
 import React, { useState }  from 'react';
 
 import Button from '../Button';
-
+import RadioGroup from '../RadioGroup';
 import './index.scss';
 
 const answers = {
-  1: 'A',
-  2: 'B',
-  3: 'B',
-  4: 'B',
-  5: 'C',
-  6: 'B',
-  7: 'A',
-  8: 'B',
-  9: 'B',
-  10: 'A',
+  1: '1A',
+  2: '2B',
+  3: '3B',
+  4: '4B',
+  5: '5C',
+  6: '6B',
+  7: '7A',
+  8: '8B',
+  9: '9B',
+  10: '10A',
 };
 
 const Quiz = () => {
@@ -24,7 +24,7 @@ const Quiz = () => {
 
     const elements = e.target.elements;
     let correct = 0;
-    Object.keys(elements).forEach(item => {
+    Object.values(answers).forEach(item => {
       const { checked, name, id } = elements[item];
       if (checked && answers[name] === id) {
         correct = correct + 1;
@@ -44,243 +44,104 @@ const Quiz = () => {
         Do you really think that you actually know everything about us?
       </div>
       <form onSubmit={submitQuiz} className="quiz-form">
-        <div className="quiz-form-field">
-          <label className="quiz-form-field-label">
-            Which coastal town on the East Coast of Australia did we meet?
-          </label>
-          <div className="quiz-form-field-options">
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="A" name="1" value="A"/>
-              <label for="A">Noosa Heads</label>
-              {/* correct answer */}
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="B" name="1" value="B"/>
-              <label for="B">Bondi Beach</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="C" name="1" value="C"/>
-              <label for="C">Cairns</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="D" name="1" value="D"/>
-              <label for="D">Surfer's paradise, Gold Coast</label>
-            </div>
-          </div>
-        </div>
-        <div className="quiz-form-field">
-          <label className="quiz-form-field-label">
-            How did they get talking?
-          </label>
-          <div className="quiz-form-field-options">
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="A" name="2" value="A"/>
-              <label for="A">Joint winners of a surfing competition</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="B" name="2" value="B"/>
-              <label for="B">Myles caught Anke in a nightclub</label>
-              {/* correct answer */}
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="C" name="2" value="C"/>
-              <label for="C">At the Ice-cream stand</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="D" name="2" value="D"/>
-              <label for="D">Dance-off competition</label>
-            </div>
-          </div>
-        </div>
-        <div className="quiz-form-field">
-          <label className="quiz-form-field-label">
-            Where did Myles propose?
-          </label>
-          <div className="quiz-form-field-options">
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="A" name="3" value="A"/>
-              <label for="A">On a beach on holiday</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="B" name="3" value="B"/>
-              <label for="B">In the New Forest at a Deer Sanctuary</label>
-              {/* correct answer */}
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="C" name="3" value="C"/>
-              <label for="C">In a restaurant in London</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="D" name="3" value="D"/>
-              <label for="D">Underwater scuba-diving</label>
-            </div>
-          </div>
-        </div>
-        <div className="quiz-form-field">
-          <label className="quiz-form-field-label">
-            How many countries have we travelled together?
-          </label>
-          
-          <div className="quiz-form-field-options">
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="A" name="4" value="A"/>
-              <label for="A">12</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="B" name="4" value="B"/>
-              <label for="B">14</label>
-              {/* correct answer */}
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="C" name="4" value="C"/>
-              <label for="C">16</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="D" name="4" value="D"/>
-              <label for="D">18</label>
-            </div>
-          </div>
-        </div>
-        <div className="quiz-form-field">
-          <label className="quiz-form-field-label">
-            What is their favourite type of holiday?
-          </label>
-          
-          <div className="quiz-form-field-options">
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="A" name="5" value="A"/>
-              <label for="A">Camping in the wild</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="B" name="5" value="B"/>
-              <label for="B">Combination of exploration and all-inclusive</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="C" name="5" value="C"/>
-              <label for="C">Beach resort holiday</label>
-              {/* correct answer */}
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="D" name="5" value="D"/>
-              <label for="D">Skiing</label>
-            </div>
-          </div>
-        </div>
-        <div className="quiz-form-field">
-          <label className="quiz-form-field-label">
-            Who plays more golf?
-          </label>
-          
-          <div className="quiz-form-field-options">
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="A" name="6" value="A"/>
-              <label for="A">Anke</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="B" name="6" value="B"/>
-              <label for="B">Myles</label>
-              {/* correct answer */}
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="C" name="6" value="C"/>
-              <label for="C">Arianna</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="D" name="6" value="D"/>
-              <label for="D">Golf is for losers</label>
-            </div>
-          </div>
-        </div>
-        <div className="quiz-form-field">
-          <label className="quiz-form-field-label">
-            Who said 'I love you' first?
-          </label>
-          
-          <div className="quiz-form-field-options">
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="A" name="7" value="A"/>
-              <label for="A">Anke</label>
-              {/* correct answer */}
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="B" name="7" value="B"/>
-              <label for="B">Myles</label>
-            </div>
-          </div>
-        </div>
-        <div className="quiz-form-field">
-          <label className="quiz-form-field-label">
-            How did the bride ask the groom out?
-          </label>
-          
-          <div className="quiz-form-field-options">
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="A" name="8" value="A"/>
-              <label for="A">Over WhatsApp</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="B" name="8" value="B"/>
-              <label for="B">Sent a letter with a return stamp</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="C" name="8" value="C"/>
-              <label for="C">Game of quiddler. I spelt out 'Will you be mine?'</label>
-              {/* correct answer */}
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="D" name="8" value="D"/>
-              <label for="D">Through a riddle</label>
-            </div>
-          </div>
-        </div>
-        <div className="quiz-form-field">
-          <label className="quiz-form-field-label">
-            How many years have they been together?
-          </label>
-          <div className="quiz-form-field-options">
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="A" name="9" value="A"/>
-              <label for="A">5 and a half years</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="B" name="9" value="B"/>
-              <label for="B">6 and a half years</label>
-              {/* correct answer */}
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="C" name="9" value="C"/>
-              <label for="C">7 and a half years</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="D" name="9" value="D"/>
-              <label for="D">8 and a half years</label>
-            </div>
-          </div>
-        </div>
-        <div className="quiz-form-field">
-          <label className="quiz-form-field-label">
-            Which are their dream holiday destinations?
-          </label>
-          <div className="quiz-form-field-options">
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="A" name="10" value="A"/>
-              <label for="A">Bali and Japan</label>
-              {/* correct answer */}
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="B" name="10" value="B"/>
-              <label for="B">Anywhere new. Need to tick those countries off!</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="C" name="10" value="C"/>
-              <label for="C">A cruise to the Northern lights</label>
-            </div>
-            <div className="quiz-form-field-options-radio">
-              <input type="radio" id="D" name="10" value="D"/>
-              <label for="D">Anywhere with a good golf course</label>
-            </div>
-          </div>
-        </div>
+        <RadioGroup 
+          title="Which coastal town on the East Coast of Australia did we meet?"
+          name="1"
+          items={[
+            {label: 'Noosa Heads', id: '1A'}, // correct answer
+            {label: 'Bondi Beach', id: '1B'},
+            {label: 'Cairns', id: '1C'},
+            {label: 'Surfer\'s paradise, Gold Coast', id: '1D'},
+          ]}
+        />
+        <RadioGroup 
+          title="How did they get talking?"
+          name="2"
+          items={[
+            {label: 'Joint winners of a surfing competition', id: '2A'},
+            {label: 'Myles caught Anke in a nightclub', id: '2B'}, // correct answer
+            {label: 'At the Ice-cream stand', id: '2C'},
+            {label: 'Dance-off competition', id: '2D'},
+          ]}
+        />
+        <RadioGroup 
+          title="Where did Myles propose?"
+          name="3"
+          items={[
+            {label: 'On a beach on holiday', id: '3A'},
+            {label: 'In the New Forest at a Deer Sanctuary', id: '3B'}, // correct answer
+            {label: 'In a restaurant in London', id: '3C'},
+            {label: 'Underwater scuba-diving', id: '3D'},
+          ]}
+        />
+        <RadioGroup 
+          title="How many countries have we travelled together?"
+          name="4"
+          items={[
+            {label: '12', id: '4A'},
+            {label: '14', id: '4B'}, // correct answer
+            {label: '16', id: '4C'},
+            {label: '18', id: '4D'},
+          ]}
+        />
+        <RadioGroup 
+          title="What is their favourite type of holiday?"
+          name="5"
+          items={[
+            {label: 'Camping in the wild', id: '5A'},
+            {label: 'Combination of exploration and all-inclusive', id: '5B'},
+            {label: 'Beach resort holiday', id: '5C'}, // correct answer
+            {label: 'Skiing', id: '5D'},
+          ]}
+        />
+        <RadioGroup 
+          title="Who plays more golf?"
+          name="6"
+          items={[
+            {label: 'Anke', id: '6A'},
+            {label: 'Myles', id: '6B'}, // correct answer
+            {label: 'Arianna', id: '6C'},
+            {label: 'Golf is for losers', id: '6D'},
+          ]}
+        />
+        <RadioGroup 
+          title="Who said 'I love you' first?"
+          name="7"
+          items={[
+            {label: 'Anke', id: '7A'}, // correct answer
+            {label: 'Myles', id: '7B'},
+          ]}
+        />
+        <RadioGroup 
+          title="How did the bride ask the groom out?"
+          name="8"
+          items={[
+            {label: 'Over WhatsApp', id: '8A'},
+            {label: 'Sent a letter with a return stamp', id: '8B'},
+            {label: 'Game of quiddler. Sge spelt out \'Will you be mine?\'', id: '8C'}, // correct answer
+            {label: 'Through a riddle', id: '8D'},
+          ]}
+        />
+        <RadioGroup 
+          title="How many years have they been together?"
+          name="9"
+          items={[
+            {label: '5 and a half years', id: '9A'},
+            {label: '6 and a half years', id: '9B'}, // correct answer
+            {label: '7 and a half years', id: '9C'},
+            {label: '8 and a half years', id: '9D'},
+          ]}
+        />
+        <RadioGroup 
+          title="Which are their dream holiday destinations?"
+          name="10"
+          items={[
+            {label: 'Bali and Japan', id: '10A'}, // correct answer
+            {label: 'Anywhere new. Need to tick those countries off!', id: '10B'},
+            {label: 'A cruise to the Northern lights', id: '10C'},
+            {label: 'Anywhere with a good golf course', id: '10D'},
+          ]}
+        />
         <div className="quiz-form-button">
           <Button type="submit" text="Submit" />
         </div>
--- src/components/RadioGroup/index.jsx
+++ src/components/RadioGroup/index.jsx
@@ -2,44 +2,25 @@ import React from 'react';
 
 import './index.scss';
 
-const RadioGroup = ({ label, children, onChange, value }) => {
-  const handleOnChange = ({ target }) => {
-    onChange(target.value)
-  };
-
-  const radioItems = children.map((child) => {
-    if (React.isValidElement(child)) {
-      return React.cloneElement(child, { 
-        onChange: handleOnChange,
-        checked: value === child.props.label,
-      });
-    }
-
-    return null;
-  });
-
+const RadioGroup = ({ title, name, items }) => {
   return (
     <div className="radio-group">
-      {label && (
+      {title && (
         <div className="radio-group-label">
-          { label }
+          { title }
         </div>
       )}
       <div className="radio-group-inputs">
-        {radioItems}
+        {items.map(({ label, id }) => (
+          <div className="radio-item">
+            <input id={id} type="radio" name={name} value={id} />
+            <label htmlFor={id}>{label}</label>
+          </div>
+        ))}
       </div>
     </div>
   );
 };
 
-const RadioItem = ({ onChange, label }) => {
-  return (
-    <div onChange={onChange} className="radio-item">
-      <input type="radio" name={label} value={label}/>
-      <label for={label}>{label}</label>
-    </div>
-  );
-};
-
 export default RadioGroup;
-export { RadioItem };
+
